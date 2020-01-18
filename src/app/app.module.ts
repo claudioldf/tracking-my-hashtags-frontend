f6d88cb46d35569c48069c32872f68a01b3daf26
claudioldf <claudioldf@gmail.com>
2020-01-17 22:28:07 -0300

    Created the project basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { ContentComponent } from './components/shared/content/content.component';
+import { MenuComponent } from './components/shared/menu/menu.component';
+import { MonitorComponent } from './components/pages/monitor/monitor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    MenuComponent,
+    MonitorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router'
+import { MonitorComponent } from './components/pages/monitor/monitor.component';
+
+export const ROUTES: Routes = [
+  { path: '', component: MonitorComponent },
+]

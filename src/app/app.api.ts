1d3b0c8479e7dbebc8026f9db72f5f8314ca8425
claudioldf <claudioldf@gmail.com>
2020-01-26 13:39:05 -0300

    Setup deploy to heroku

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,1 +1,3 @@
-export const BASE_URL_API = 'http://localhost:3000';
+import { environment } from './../environments/environment';
+
+export const BASE_URL_API = environment.apiUrl;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  enableDebug: false,
+  apiUrl: 'https://tracking-my-hashtags.herokuapp.com',
 };

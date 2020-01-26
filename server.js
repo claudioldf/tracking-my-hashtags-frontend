70530b956940448f8e081cd7cf115f2703daa850
claudioldf <claudioldf@gmail.com>
2020-01-26 15:13:48 -0300

    fixing cors / add server to serve the angular assets

---
+++ server.js
@@ -0,0 +1,25 @@
+const cors = require('cors');
+const express = require('express');
+const app = express();
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+const port = process.env.PORT || 8080;
+
+app.use(forceSSL());
+app.use(cors());
+app.use(express.static(__dirname + '/dist'));
+
+app.options('*', cors())
+app.listen(port);
+
+console.log("Listening on *:" + port);
